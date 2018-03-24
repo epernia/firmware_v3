ecf8ac4bbe7662a963e20c40c6595be954befd09
Eric Pernia <ericpernia@hotmail.com>
2018-03-24 01:51:01 -0300

    repair C style

--- examples/CPP_sAPI/inc/cxx_wrapper.hpp
+++ examples/CPP_sAPI/inc/cxx_wrapper.hpp
@@ -9,84 +9,84 @@ namespace sapi
 class GpioOut
 {
 private:
-    gpioMap_t _pin;
+   gpioMap_t _pin;
 public:
-    GpioOut(gpioMap_t pin): _pin(pin)
-    {
-        gpioConfig(_pin, GPIO_OUTPUT);
-    }
-    GpioOut& operator=(int v)
-    {
-        gpioWrite(_pin, v);
-        return *this;
-    }
-    inline operator int() const
-    {
-        return gpioRead(_pin);
-    }
-    GpioOut& toggle()
-    {
-        *this = inverted();
-        return *this;
-    }
-    bool isOn() const
-    {
-        return int(*this);
-    }
-    bool isOff() const
-    {
-        return !isOn();
-    }
-    int inverted() const
-    {
-        return !int(*this);
-    }
+   GpioOut(gpioMap_t pin): _pin(pin)
+   {
+      gpioConfig(_pin, GPIO_OUTPUT);
+   }
+   GpioOut& operator=(int v)
+   {
+      gpioWrite(_pin, v);
+      return *this;
+   }
+   inline operator int() const
+   {
+      return gpioRead(_pin);
+   }
+   GpioOut& toggle()
+   {
+      *this = inverted();
+      return *this;
+   }
+   bool isOn() const
+   {
+      return int(*this);
+   }
+   bool isOff() const
+   {
+      return !isOn();
+   }
+   int inverted() const
+   {
+      return !int(*this);
+   }
 };
 
 class GpioIn
 {
 private:
-    gpioMap_t _pin;
+   gpioMap_t _pin;
 public:
-    GpioIn(gpioMap_t pin) : _pin(pin)
-    {
-        gpioConfig(_pin, GPIO_INPUT);
-    }
-    operator int() const
-    {
-        return gpioRead(_pin);
-    }
-    bool isOn() const
-    {
-        return int(*this);
-    }
-    bool isOff() const
-    {
-        return !isOn();
-    }
-    int inverted() const
-    {
-        return !int(*this);
-    }
+   GpioIn(gpioMap_t pin) : _pin(pin)
+   {
+      gpioConfig(_pin, GPIO_INPUT);
+   }
+   operator int() const
+   {
+      return gpioRead(_pin);
+   }
+   bool isOn() const
+   {
+      return int(*this);
+   }
+   bool isOff() const
+   {
+      return !isOn();
+   }
+   int inverted() const
+   {
+      return !int(*this);
+   }
 };
 
 class Board_t
 {
 public:
-    Board_t();
+   Board_t();
 
-    float frec() const
-    {
-        SystemCoreClockUpdate();
-        return SystemCoreClock / 1000.0f;
-    }
+   float frec() const
+   {
+      SystemCoreClockUpdate();
+      return SystemCoreClock / 1000.0f;
+   }
 };
 
 extern Board_t Board;
 
 template<typename T, int N> int arraySize(T(&)[N])
 {
-    return N;
+   return N;
 }
 
 }
