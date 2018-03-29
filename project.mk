818bbcbe3aba12040cd3a8185ca068bc4eab65b0
Eric Pernia <ericpernia@hotmail.com>
2018-03-28 21:47:51 -0300

    add documentation and test several eamples

--- project.mk
+++ project.mk
@@ -5,18 +5,21 @@
 # ---------- Project path ----------------------------------------------
 # Project path 
 # (relative to this folder, leave void if the project is in this folder)
-#PROJECT_PATH =
+PROJECT_PATH =
 
 # ---------- Project names ---------------------------------------------
-#PROJECT_NAME = myProject
+PROJECT_NAME = myProject
 
 
 #==============================================================================
 # Examples
+# Notas: Los proyectos indentados compilan pero no se probo con el periferico
+#        desde el pasaje de Firmware v2 a este repo.
+#        Los proyectos marcados con ################# no compilan.
 #==============================================================================
 
 #------------------------------------------------------------------------------
-# languages_and_libs
+# Languages and libs
 #------------------------------------------------------------------------------
 
 # ---------- Projects path ---------------------------------------------
@@ -35,32 +38,40 @@
 # sAPI
 #------------------------------------------------------------------------------
 
-	# ---------- Projects path ---------------------------------------------
-	#PROJECT_PATH = examples/sAPI/bare_metal
-	# ---------- Project names (uncomment only one) ------------------------
+# ---------- Projects path ---------------------------------------------
+#PROJECT_PATH = examples/sAPI/bare_metal
+# ---------- Project names (uncomment only one) ------------------------
 	#PROJECT_NAME = adc_dac_01
-	#PROJECT_NAME = cycles_counter_01
+#PROJECT_NAME = cycles_counter_01
 	#PROJECT_NAME = keypad_7segment_01
 	#PROJECT_NAME = lcd_01
-	#PROJECT_NAME = rtc_01
+#PROJECT_NAME = rtc_01
 	#PROJECT_NAME = servo_01
-	#PROJECT_NAME = spi_01_sdCard_fatFileSystem
-	#PROJECT_NAME = stdio_01_printf_sprintf
-	#PROJECT_NAME = tick_01_tickHook
-
-	# ---------- Projects path ---------------------------------------------
-	#PROJECT_PATH = examples/sAPI/bare_metal/gpio
-	# ---------- Project names (uncomment only one) ------------------------
-	#PROJECT_NAME = gpio_01_switches_leds
-	#PROJECT_NAME = gpio_02_blinky
-	#PROJECT_NAME = gpio_03_blinky_switch
-	#PROJECT_NAME = gpio_04_led_sequences
+		# Nota: Este ejemplo de servo puede requerir un borrado completo de
+		# la memoria flash luego de su uso para grabar programas posteriores.
+		# Para hcerlo debe puentear el Jumper JP5 y resetear la placa con el
+		# puente sostenido hasta soltar el boton de reset entrando en modo
+		# ISP (se prenden todos los leds), luego aplique "make erase" y debe
+		# resetear nuevamente la placa para permitir grabar nuevos programas.
+		# Tenga en cuenta tambien la corriente maxima del regulador al
+		# conectar uno o mas servos.
+	#################PROJECT_NAME = spi_01_sdCard_fatFileSystem # falta biblioteca de fileSystem
+#PROJECT_NAME = stdio_01_printf_sprintf
+#PROJECT_NAME = tick_01_tickHook
 
 # ---------- Projects path ---------------------------------------------
-#PROJECT_PATH = examples/sAPI/bare_metal/i2c
+#PROJECT_PATH = examples/sAPI/bare_metal/gpio
 # ---------- Project names (uncomment only one) ------------------------
-##################PROJECT_NAME = i2c_02_qmc5883l
-###################PROJECT_NAME = i2c_01_hmc5883l
+#PROJECT_NAME = gpio_01_switches_leds
+#PROJECT_NAME = gpio_02_blinky
+#PROJECT_NAME = gpio_03_blinky_switch
+#PROJECT_NAME = gpio_04_led_sequences
+
+	# ---------- Projects path ---------------------------------------------
+	#PROJECT_PATH = examples/sAPI/bare_metal/i2c
+	# ---------- Project names (uncomment only one) ------------------------
+	#PROJECT_NAME = i2c_02_qmc5883l
+	#PROJECT_NAME = i2c_01_hmc5883l
 
 # ---------- Projects path ---------------------------------------------
 #PROJECT_PATH = examples/sAPI/bare_metal/pwm
@@ -68,10 +79,10 @@
 #PROJECT_NAME = pwm_01
 #PROJECT_NAME = pwm_02_rgb_controller_uart
 
-# ---------- Projects path ---------------------------------------------
-#PROJECT_PATH = examples/sAPI/bare_metal/temperature_humidity_sensors
-# ---------- Project names (uncomment only one) ------------------------
-#PROJECT_NAME = dht11_01
+	# ---------- Projects path ---------------------------------------------
+	#PROJECT_PATH = examples/sAPI/bare_metal/temperature_humidity_sensors
+	# ---------- Project names (uncomment only one) ------------------------
+	#PROJECT_NAME = dht11_01
 
 # ---------- Projects path ---------------------------------------------
 #PROJECT_PATH = examples/sAPI/bare_metal/uart
@@ -80,67 +91,67 @@
 #PROJECT_NAME = uart_02_receive_string_blocking
 #PROJECT_NAME = uart_03_receive_string
 
-# ---------- Projects path ---------------------------------------------
-#PROJECT_PATH = examples/sAPI/bare_metal/ultrasonicSensors
-# ---------- Project names (uncomment only one) ------------------------
-#PROJECT_NAME = ultrasonicSensor_HCSR04_01
+	# ---------- Projects path ---------------------------------------------
+	#PROJECT_PATH = examples/sAPI/bare_metal/ultrasonicSensors
+	# ---------- Project names (uncomment only one) ------------------------
+	#PROJECT_NAME = ultrasonicSensor_HCSR04_01
+
+	# ---------- Projects path ---------------------------------------------
+	#PROJECT_PATH = examples/sAPI/bare_metal/ultrasonicSensors
+	# ---------- Project names (uncomment only one) ------------------------
+	#PROJECT_NAME = ultrasonicSensor_HCSR04_01
 
 # ---------- Projects path ---------------------------------------------
 #PROJECT_PATH = examples/sAPI/bare_metal/wifi_esp01_esp8266
-# ---------- Project names (uncomment only one) ------------------------
-#PROJECT_NAME = ultrasonicSensor_HCSR04_01
-
-# ---------- Projects path ---------------------------------------------
-#PROJECT_PATH = examples/sAPI/operating_systems/coperative
 # ---------- Project names (uncomment only one) ------------------------
 #PROJECT_NAME = 01_uart_bridge
 #PROJECT_NAME = 02_http_server
 #PROJECT_NAME = 03_thingspeak
 
 
-# ---------- Projects path ---------------------------------------------
-#PROJECT_PATH = examples/sAPI/operating_systems/freeRTOS
-# ---------- Project names (uncomment only one) ------------------------
-#PROJECT_NAME = freeRTOS_01_blinky
-#PROJECT_NAME = freeRTOS_02_Queue
-#PROJECT_NAME = freeRTOS_03_ChanFatFS_SPI_SdCard_ADC_log
+	# ---------- Projects path ---------------------------------------------
+	#PROJECT_PATH = examples/sAPI/operating_systems/freeRTOS
+	# ---------- Project names (uncomment only one) ------------------------
+	#PROJECT_NAME = freeRTOS_01_blinky
+	#PROJECT_NAME = freeRTOS_02_Queue
+	#PROJECT_NAME = freeRTOS_03_ChanFatFS_SPI_SdCard_ADC_log
 
-# ---------- Projects path ---------------------------------------------
-#PROJECT_PATH = examples/sAPI/operating_systems/freeRTOS/freeRTOS_book
-# ---------- Project names (uncomment only one) ------------------------
-#PROJECT_NAME = Example001
-#PROJECT_NAME = Example002
-#PROJECT_NAME = Example003
-#PROJECT_NAME = Example004
-#PROJECT_NAME = Example005
-#PROJECT_NAME = Example006
-#PROJECT_NAME = Example007
-#PROJECT_NAME = Example008
-#PROJECT_NAME = Example009
-#PROJECT_NAME = Example010
-#PROJECT_NAME = Example011
-#PROJECT_NAME = Example012
-#PROJECT_NAME = Example013
-#PROJECT_NAME = Example014
-#PROJECT_NAME = Example015
-#PROJECT_NAME = Example016
-#PROJECT_NAME = Example017
-#PROJECT_NAME = Example018
-#PROJECT_NAME = Example019
-#PROJECT_NAME = Example020
-#PROJECT_NAME = Example021
-#PROJECT_NAME = Example022
+	# ---------- Projects path ---------------------------------------------
+	#PROJECT_PATH = examples/sAPI/operating_systems/freeRTOS/freeRTOS_book
+	# ---------- Project names (uncomment only one) ------------------------
+	#PROJECT_NAME = Example001
+	#PROJECT_NAME = Example002
+	#PROJECT_NAME = Example003
+	#PROJECT_NAME = Example004
+	#PROJECT_NAME = Example005
+	#PROJECT_NAME = Example006
+	#PROJECT_NAME = Example007
+	#PROJECT_NAME = Example008
+	#PROJECT_NAME = Example009
+	#PROJECT_NAME = Example010
+	#PROJECT_NAME = Example011
+	#PROJECT_NAME = Example012
+	#PROJECT_NAME = Example013
+	#PROJECT_NAME = Example014
+	#PROJECT_NAME = Example015
+	#PROJECT_NAME = Example016
+	#PROJECT_NAME = Example017
+	#PROJECT_NAME = Example018
+	#PROJECT_NAME = Example019
+	#PROJECT_NAME = Example020
+	#PROJECT_NAME = Example021
+	#PROJECT_NAME = Example022
 
 # ---------- Projects path ---------------------------------------------
 #################PROJECT_PATH = examples/sAPI/operating_systems/freeOSEK
 # ---------- Project names (uncomment only one) ------------------------
-#PROJECT_NAME = freeOSEK_01_blinky
+#################PROJECT_NAME = freeOSEK_01_blinky # OSEK aun no esta portado
 
 
 # ---------- Projects path ---------------------------------------------
 #PROJECT_PATH = examples/sAPI/statecharts
 # ---------- Project names (uncomment only one) ------------------------
-#PROJECT_NAME = statecharts_bare_metal
+#################PROJECT_NAME = statecharts_bare_metal
 
 # ---------- Projects path ---------------------------------------------
 #PROJECT_PATH = examples/sAPI/statecharts/statecharts_os/coperative_os_examples
@@ -159,25 +170,25 @@
 
 
 #------------------------------------------------------------------------------
-# lpcOpen
+# LPC Open
 #------------------------------------------------------------------------------
 
 # ---------- Projects path ---------------------------------------------
 #PROJECT_PATH = examples/lpcOpen
 # ---------- Project names (uncomment only one) ------------------------
-#PROJECT_NAME = adc_fir_dac
+##########################PROJECT_NAME = adc_fir_dac   # arreglar el makefile
 #PROJECT_NAME = blinky
 #PROJECT_NAME = blinky_ram
 #PROJECT_NAME = blinky_rit
-#PROJECT_NAME = boot
-#PROJECT_NAME = freertos_blinky
-#PROJECT_NAME = i2c
-#PROJECT_NAME = mpu
-#PROJECT_NAME = statechart
+##########################PROJECT_NAME = boot
+##########################PROJECT_NAME = freertos_blinky
+#PROJECT_NAME = i2c	# Solo se testeo compilacion
+##########################PROJECT_NAME = mpu
+##########################PROJECT_NAME = statechart
 
 # ---------- Projects path ---------------------------------------------
 #PROJECT_PATH = examples/lpcOpen/asm
 # ---------- Project names (uncomment only one) ------------------------
-#PROJECT_NAME = first
-#PROJECT_NAME = conv
-#PROJECT_NAME = packer
+##########################PROJECT_NAME = first
+##########################PROJECT_NAME = conv
+##########################PROJECT_NAME = packer
