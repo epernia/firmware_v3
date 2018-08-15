ce99571d2af3a982122a5c6d0947bd56a62faa3a
Eric Pernia <ericpernia@gmail.com>
2018-08-14 23:02:57 -0300

    fix http server example

--- project.mk
+++ project.mk
@@ -31,9 +31,12 @@
 #PROJECT_PATH = examples/c/sapi/bare_metal/bluetooth
 #PROJECT_NAME = hm10_uart_bridge
 
-PROJECT_PATH = examples/c/sapi/bare_metal/i2c/IMUs
-PROJECT_NAME = mpu9250
+#PROJECT_PATH = examples/c/sapi/bare_metal/i2c/IMUs
+#PROJECT_NAME = mpu9250
 
 #PROJECT_PATH = examples/c/sapi/bare_metal/usb
 #PROJECT_NAME = usb_device_01_hid_keyboard
 #PROJECT_NAME = usb_device_02_cdc_virtualSerialPort
+
+PROJECT_PATH = examples/c/sapi/bare_metal/wifi_esp01_esp8266
+PROJECT_NAME = 02_http_server
