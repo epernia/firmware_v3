650af1df28085950c3173679b22b6909bb95479c
Eric Pernia <ericpernia@gmail.com>
2018-09-01 13:43:34 -0300

    update project.mk

--- project.mk
+++ project.mk
@@ -13,44 +13,6 @@
 #PROJECT_PATH = examples/c/sapi/bare_metal
 #PROJECT_NAME = sapi_basic_example
 
-#PROJECT_PATH = examples/c/sapi/bare_metal
-#PROJECT_NAME = sd_logger
-
 # ---------- Example of path outside this file folder ------------------
 #PROJECT_PATH = ../examples/c/sapi/bare_metal
 #PROJECT_NAME = sapi_basic_example
-
-
-
-#PROJECT_PATH = examples/c/sapi/operating_systems/free_rtos/dynamic_mem
-#PROJECT_NAME = freeRTOS_03_ChanFatFS_SPI_SdCard_ADC_log
-
-#PROJECT_PATH = examples/c
-#PROJECT_NAME = newlib_printf_scanf
-
-#PROJECT_PATH = examples/c/sapi/bare_metal/bluetooth
-#PROJECT_NAME = hm10_uart_bridge
-
-#PROJECT_PATH = examples/c/sapi/bare_metal/i2c/IMUs
-#PROJECT_NAME = mpu9250
-
-#PROJECT_PATH = examples/c/sapi/bare_metal/temperature_humidity_sensors
-#PROJECT_NAME = dht11_01
-
-#PROJECT_PATH = examples/c/sapi/bare_metal
-#PROJECT_NAME = lcd_01
-
-#PROJECT_PATH = examples/c/sapi/bare_metal/spi_sd_card
-#PROJECT_NAME = spi_sd_card_01_wite_file
-#PROJECT_NAME = spi_sd_card_02_log_time_stamp
-#PROJECT_NAME = spi_sd_card_03_list_files
-
-#PROJECT_PATH = examples/c/sapi/bare_metal/usb
-#PROJECT_NAME = usb_device_01_hid_keyboard
-#PROJECT_NAME = usb_device_02_cdc_virtualSerialPort
-
-
-
-
-#PROJECT_PATH = examples/c/lpc_open/bare_metal
-#PROJECT_NAME = usb_msc_host
