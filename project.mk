17981806c14be15b47234e95d53aad0a9d450c0e
Eric Pernia <ericpernia@gmail.com>
2018-09-01 14:08:28 -0300

    update gitignore

---
+++ project.mk
@@ -0,0 +1,18 @@
+#==============================================================================
+# Select a project to compile, or comment and uncomment an example
+#==============================================================================
+
+# ---------- Project path ----------------------------------------------
+# Project path 
+# (relative to this folder, leave void if the project is in this folder)
+#PROJECT_PATH =
+# ---------- Project names ---------------------------------------------
+#PROJECT_NAME = app
+
+# ---------- Example of path inside this file folder -------------------
+#PROJECT_PATH = examples/c/sapi/bare_metal
+#PROJECT_NAME = sapi_basic_example
+
+# ---------- Example of path outside this file folder ------------------
+#PROJECT_PATH = ../examples/c/sapi/bare_metal
+#PROJECT_NAME = sapi_basic_example
