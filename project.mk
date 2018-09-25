34f4f5b2b0fee5aa8754e7f22c38ff72d2fa3232
Eric Pernia <ericpernia@gmail.com>
2018-09-24 21:43:40 -0300

    update project.mk

--- project.mk
+++ project.mk
@@ -16,10 +16,3 @@
 # ---------- Example of path outside this file folder ------------------
 #PROJECT_PATH = ../examples/c/sapi/bare_metal
 #PROJECT_NAME = sapi_basic_example
-
-PROJECT_PATH = examples/c/sapi/bare_metal/usb
-PROJECT_NAME = usb_device_03_hid_numerical_keypad
-#PROJECT_NAME = usb_device_02_cdc_virtualSerialPort
-
-#PROJECT_PATH = examples/c
-#PROJECT_NAME = newlib_printf_scanf
