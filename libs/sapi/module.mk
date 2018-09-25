5ddb423a298c6c362cf85089dcffbc5fa83d91f3
Eric Pernia <ericpernia@gmail.com>
2018-09-25 09:41:35 -0300

    update sapi order

--- libs/sapi/module.mk
+++ libs/sapi/module.mk
@@ -1,8 +1,21 @@
 ifeq ($(USE_SAPI),y)
 
-SAPI_BASE=libs/sapi
+SAPI_BASE=libs/sapi/sapi_v0.5.2
+
 DEFINES+=USE_SAPI
-INCLUDES += -I$(SAPI_BASE)/sapi_v0.5.1/inc
-SRC+=$(wildcard $(SAPI_BASE)/sapi_v0.5.1/src/*.c)
+
+INCLUDES += -I$(SAPI_BASE)/base/inc
+INCLUDES += -I$(SAPI_BASE)/soc/core/inc
+INCLUDES += -I$(SAPI_BASE)/soc/peripherals/inc
+INCLUDES += -I$(SAPI_BASE)/board/inc
+INCLUDES += -I$(SAPI_BASE)/abstract_modules/inc
+INCLUDES += -I$(SAPI_BASE)/external_peripherals/inc
+
+SRC += $(wildcard $(SAPI_BASE)/base/src/*.c)
+SRC += $(wildcard $(SAPI_BASE)/soc/core/src/*.c)
+SRC += $(wildcard $(SAPI_BASE)/soc/peripherals/src/*.c)
+SRC += $(wildcard $(SAPI_BASE)/board/src/*.c)
+SRC += $(wildcard $(SAPI_BASE)/abstract_modules/src/*.c)
+SRC += $(wildcard $(SAPI_BASE)/external_peripherals/src/*.c)
 
 endif
