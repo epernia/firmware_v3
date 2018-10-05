bcfa4cfcee429c3f555be381c055b09833bfc770
Eric Pernia <ericpernia@gmail.com>
2018-10-05 17:23:54 -0300

    change examples order

--- project.mk
+++ project.mk
@@ -5,15 +5,15 @@
 # ---------- Example of path inside this file folder -------------------
 # Project path
 #(relative to this folder, leave void if the project is in this folder)
-#PROJECT_PATH = examples/c/sapi/bare_metal/gpio
+#PROJECT_PATH = examples/c/sapi/gpio
 # Project name
-#PROJECT_NAME = gpio_blinky
+#PROJECT_NAME = blinky
 
 # ---------- Example of path outside this file folder ------------------
 # Project path
 #PROJECT_PATH = ../examples
 # Project name
-#PROJECT_NAME = example
+#PROJECT_NAME = blinky
 
 PROJECT_PATH = 
 PROJECT_NAME = app
