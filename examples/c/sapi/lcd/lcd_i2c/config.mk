b5ec864fab06254afd36900da8da359f986aea4d
Eric Pernia <ericpernia@gmail.com>
2019-06-16 06:55:32 -0300

    improve clean_all.sh script

---
+++ examples/c/sapi/lcd/lcd_i2c/config.mk
@@ -0,0 +1,13 @@
+# Compile options
+VERBOSE=n
+OPT=g
+USE_NANO=n
+SEMIHOST=n
+USE_FPU=y
+
+# Libraries
+USE_LPCOPEN=y
+USE_SAPI=y
+
+# For LCD connected via I2C PCF8574T I/O expander
+DEFINES+=LCD_HD44780_I2C_PCF8574T
