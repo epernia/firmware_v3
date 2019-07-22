e3831086acbab25069f7f7f8f23cc2a0a2ccb1b8
Eric Pernia <ericpernia@gmail.com>
2019-07-22 11:16:30 -0300

    update templates

--- templates/module/module_en_with_examples/inc/moduleName.h
+++ templates/module/module_en_with_examples/inc/moduleName.h
@@ -15,6 +15,9 @@
 
 /*=====[Inclusions of public function dependencies]==========================*/
 
+#include "dependency.h"
+#include <dependency.h>
+
 /*=====[C++ - begin]=========================================================*/
 
 #ifdef __cplusplus
@@ -23,13 +26,56 @@ extern "C" {
 
 /*=====[Definition macros of public constants]===============================*/
 
+#define PI 3.14
+
 /*=====[Public function-like macros]=========================================*/
+
+#define printInt(printer,number) printIntFormat((printer),(number),(DEC_FORMAT))
+
+#define printlnString(printer,string);   {printString((printer),(string));\
+                                          printEnter((printer));}
 
 /*=====[Definitions of public data types]====================================*/
 
+// Data type that renames an elementary data type
+typedef uint8_t bool_t;
+
+// Function pointer data type
+typedef void (*callBackFuncPtr_t)(void *);
+
+// Enumerated data type
+typedef enum {
+   GPIO_INPUT, 
+   GPIO_OUTPUT
+} gpioMode_t;
+
+typedef enum {
+   HMC5883L_REG_X_MSB = 0x00, 
+   HMC5883L_REG_X_LSB = 0x01,
+   HMC5883L_REG_Y_MSB = 0x02,
+   HMC5883L_REG_Y_LSB = 0x03,
+   HMC5883L_REG_Z_MSB = 0x04,
+   HMC5883L_REG_Z_LSB = 0x05
+} hmc5883lRegisters_t;
+
+// Structured, union or bit field data type
+typedef struct {
+   uint16_t year;	 /* 1 to 4095 */
+   uint8_t  month; /* 1 to 12   */
+   uint8_t  mday;	 /* 1 to 31   */
+   uint8_t  wday;	 /* 1 to 7    */
+   uint8_t  hour;	 /* 0 to 23   */
+   uint8_t  min;	 /* 0 to 59   */
+   uint8_t  sec;	 /* 0 to 59   */
+} rtc_t;
+
 /*=====[Prototypes (declarations) of public functions]=======================*/
 
+bool_t rtcInit( rtc_t* rtc );
+
 /*=====[Prototypes (declarations) of public interrupt functions]=============*/
+
+void UART0_IRQHandler(void);
 
 /*=====[C++ - end]===========================================================*/
 
--- templates/module/module_en_with_examples/src/moduleName.c
+++ templates/module/module_en_with_examples/src/moduleName.c
@@ -10,26 +10,80 @@
  
 /*=====[Inclusion of own header]=============================================*/
 
-#include <moduleName.h>
-
+#include "moduleName.h"
+ 
 /*=====[Inclusions of private function dependencies]=========================*/
+
+#include "dependency.h"
+#include <dependency.h>
 
 /*=====[Definition macros of private constants]==============================*/
 
+#define QMC5883L_REG_Y_LSB       0x02
+#define MAX_AMOUNT_OF_RGB_LEDS   9
+
 /*=====[Private function-like macros]========================================*/
+
+#define rtcConfig rtcInit
 
 /*=====[Definitions of private data types]===================================*/
 
+// Data type that renames an elementary data type
+typedef uint32_t gpioRegister_t;
+
+// Function pointer data type
+typedef void (*FuncPtrPrivado_t)(void *);
+
+// Enumerated data type
+typedef enum {
+   GPIO_INPUT_PULLUP, 
+   GPIO_INPUT_PULLDOWN,
+   GPIO_REPEATER
+} gpioLPC4337Modes_t;
+
+// Structured, union or bit field data type
+typedef struct {
+   uint8_t scuPort;
+   uint8_t scuPin;
+   uint8_t func;
+   uint8_t gpioPort;
+   uint8_t gpioPin;
+} gpioLpc4337_t;
+
 /*=====[Definitions of external public global variables]=====================*/
+
+extern int32_t varGlobalExterna;
 
 /*=====[Definitions of public global variables]==============================*/
 
+int32_t varGlobalPublica = 0;
+
 /*=====[Definitions of private global variables]=============================*/
+
+static int32_t varGlobalPrivada = 0;
 
 /*=====[Prototypes (declarations) of private functions]======================*/
 
+static void funPrivada(void);
+
 /*=====[Implementations of public functions]=================================*/
+
+bool_t rtcInit( rtc_t* rtc )
+{
+   // ...
+}
 
 /*=====[Implementations of interrupt functions]==============================*/
 
+void UART0_IRQHandler(void)
+{
+   // ...
+}
+
 /*=====[Implementations of private functions]================================*/
+
+static void funPrivada(void)
+{
+   // ...
+}
+
