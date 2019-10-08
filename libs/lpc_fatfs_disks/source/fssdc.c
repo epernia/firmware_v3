93fc82922b045f8a827e26ae11d6f7ec542b84ee
Santiago Germino <royconejo@gmail.com>
2019-10-08 03:42:20 -0300

    Fixes on DAC/ADC PID example

--- libs/lpc_fatfs_disks/source/fssdc.c
+++ libs/lpc_fatfs_disks/source/fssdc.c
@@ -35,7 +35,6 @@
 /*  ELM-Chan Says:
  
     On Initialization: Set SPI clock rate between 100 kHz and 400 kHz.
-    
     After: The SCLK rate should be changed to fast as possible to maximize the 
     read/write performance. The TRAN_SPEED field in the CSD register indicates 
     the maximum clock rate of the card. The maximum clock rate is 20MHz for MMC,
@@ -59,14 +58,14 @@
 #endif
 
 #ifndef FSSDC_SPI_FAST_CLOCK
-#define FSSDC_SPI_FAST_CLOCK            25000000
+#define FSSDC_SPI_FAST_CLOCK            15000000
 #endif
 
 
 /* Definitions for MMC/SDC command */
 #define CMD0_	(0x40+0)	 /* GO_IDLE_STATE */
-#define CMD1	(0x40+1)     /* SEND_OP_COND (MMC) */
-#define	ACMD41	(0xC0+41)    /* SEND_OP_COND (SDC) */
+#define CMD1	(0x40+1)	 /* SEND_OP_COND (MMC) */
+#define	ACMD41	(0xC0+41)	 /* SEND_OP_COND (SDC) */
 #define CMD8	(0x40+8)	 /* SEND_IF_COND */
 #define CMD9	(0x40+9)	 /* SEND_CSD */
 #define CMD10	(0x40+10)	 /* SEND_CID */
@@ -76,7 +75,7 @@
 #define CMD17	(0x40+17)	 /* READ_SINGLE_BLOCK */
 #define CMD18	(0x40+18)	 /* READ_MULTIPLE_BLOCK */
 #define CMD23	(0x40+23)	 /* SET_BLOCK_COUNT (MMC) */
-#define ACMD23  (0xC0+23)    /* SET_WR_BLK_ERASE_COUNT (SDC) */
+#define ACMD23	(0xC0+23)	 /* SET_WR_BLK_ERASE_COUNT (SDC) */
 #define CMD24	(0x40+24)	 /* WRITE_BLOCK */
 #define CMD25	(0x40+25)	 /* WRITE_MULTIPLE_BLOCK */
 #define CMD55	(0x40+55)	 /* APP_CMD */
