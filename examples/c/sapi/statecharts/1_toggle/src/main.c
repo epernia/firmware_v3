005ae1e338fad1403fa3098dfe9844007f61fc2d
Juan Manuel Cruz <juanmanuelcruzbeaufrere@gmail.com>
2020-02-04 15:57:39 -0300

    Add 1_toggle => Statechart example using sysTick

---
+++ examples/c/sapi/statecharts/1_toggle/src/main.c
@@ -0,0 +1,157 @@
+/* Copyright 2017, Pablo Ridolfi, Juan Manuel Cruz
+ * All rights reserved.
+ *
+ * This file is part of Workspace.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright notice,
+ *    this list of conditions and the following disclaimer.
+ *
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ *
+ * 3. Neither the name of the copyright holder nor the names of its
+ *    contributors may be used to endorse or promote products derived from this
+ *    software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ *
+ */
+ 
+/** @brief This is a simple statechart example using Yakindu Statechart Tool
+ * Plug-in (update site: http://updates.yakindu.org/sct/mars/releases/).
+ */
+
+/** \addtogroup statechart Simple UML Statechart example.
+ ** @{ */
+
+/*==================[inclusions]=============================================*/
+
+#include "sapi.h"       // <= sAPI header
+
+/* Include statechart header file. Be sure you run the statechart C code
+ * generation tool!
+ */
+#include "Toggle.h"
+
+
+/*==================[macros and definitions]=================================*/
+
+#define TICKRATE_1MS	(1)				/* 1000 ticks per second */
+#define TICKRATE_MS		(TICKRATE_1MS)	/* 1000 ticks per second */
+
+
+/*==================[internal data declaration]==============================*/
+
+volatile bool SysTick_Time_Flag = false;
+
+/*! This is a state machine */
+static Toggle statechart;
+
+
+/*==================[internal functions declaration]=========================*/
+
+/*==================[internal data definition]===============================*/
+
+/*==================[external data definition]===============================*/
+
+/*==================[internal functions definition]==========================*/
+
+/*==================[external functions definition]==========================*/
+
+/*! \file This header defines prototypes for all functions that are required
+ *  by the state machine implementation.
+ *
+ *  This state machine makes use of operations declared in the state machines
+ *  interface or internal scopes. Thus the function prototypes:
+ *  - toggleIface_opLED
+ *  are defined.
+ *
+ *  These functions will be called during a 'run to completion step' (runCycle)
+ *  of the statechart.
+ *  There are some constraints that have to be considered for the
+ *  implementation of these functions:
+ *  - never call the statechart API functions from within these functions.
+ *  - make sure that the execution time is as short as possible.
+ */
+
+/** state machine user-defined external function (action)
+ *
+ * @param handle state machine instance
+ * @param LEDNumber number of LED
+ * @param onoff state machine operation parameter
+ */
+void toggleIface_opLED( Toggle* handle, sc_integer LEDNumber )
+{
+	gpioToggle( (LEDR + LEDNumber) );
+}
+
+
+/**
+ * @brief	Hook on Handle interrupt from SysTick timer
+ * @return	Nothing
+ */
+void myTickHook( void *ptr ){
+
+	/* The sysTick Interrupt Handler only set a Flag */
+	SysTick_Time_Flag = true;
+}
+
+
+/**
+ * @brief	main routine for statechart example: EDU-CIAA-NXP - Toggle LED3
+ * @return	Function should not exit.
+ */
+int main(void)
+{
+	/* Generic Initialization */
+	boardConfig();
+
+	/* Init Ticks counter => TICKRATE_MS */
+	tickConfig( TICKRATE_MS );
+
+	/* Add Tick Hook */
+	tickCallbackSet( myTickHook, (void*)NULL );
+
+	/* Statechart Initialization */
+	toggle_init( &statechart );
+	toggle_enter( &statechart );
+
+	/* LED state is toggled in the main program */
+	while (1) {
+		/* The uC sleeps waiting for an interruption */
+		__WFI();
+
+		/* When a interrupt wakes to the uC, the main program validates it,
+		 * checking the waited Flag */
+		if (SysTick_Time_Flag == true) {
+
+			/* Then reset its Flag */
+			SysTick_Time_Flag = false;
+
+			/* Then Raise an Event -> evTick => OK */
+			toggleIface_raise_evTick( &statechart );
+
+			/* Then Run an Cycle of Statechart */
+			toggle_runCycle( &statechart );
+		}
+	}
+}
+
+
+/** @} doxygen end group definition */
+
+/*==================[end of file]============================================*/
