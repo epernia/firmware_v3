a10c2d413c2fd3694c38470474d3474b6805d944
David Broin <davidmbroin@gmail.com>
2020-05-19 12:45:33 -0500

    Add example Preprocessing assembly code

---
+++ examples/asm/asm_c_directives/src/main.c
@@ -0,0 +1,92 @@
+/* Copyright 2018, Facundo Larosa - Danilo Zecchin
+ * All rights reserved.
+ *
+ * This file is part of arquitecturaDeMicroprocesadores.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright notice,
+ *    this list of conditions and the following disclaimer.
+ *
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ *
+ * 3. Neither the name of the copyright holder nor the names of its
+ *    contributors may be used to endorse or promote products derived from this
+ *    software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ *
+ */
+
+/** @brief This is a simple blink example.
+ */
+
+/** \addtogroup blink Bare-metal blink example
+ ** @{ */
+
+/*==================[inclusions]=============================================*/
+
+#include "main.h"
+#include "board.h"
+#include "suma.h"
+
+/*==================[macros and definitions]=================================*/
+
+/*==================[internal data declaration]==============================*/
+
+/*==================[internal functions declaration]=========================*/
+
+/** @brief hardware initialization function
+ *	@return none
+ */
+static void initHardware(void);
+
+/*==================[internal data definition]===============================*/
+
+/*==================[external data definition]===============================*/
+
+/*==================[internal functions definition]==========================*/
+
+static void initHardware(void)
+{
+	Board_Init();
+	SystemCoreClockUpdate();
+	//SysTick_Config(SystemCoreClock / 1000);
+}
+
+/*==================[external functions definition]==========================*/
+
+int main(void)
+{
+	volatile  uint32_t aValue = 20,
+			 otherValue = 30,
+			 sumResult_Asm,sumResult_C;
+
+	initHardware();
+
+	sumResult_C= CSum(aValue,otherValue);
+
+	sumResult_Asm = asmSum(aValue, otherValue);
+
+	while (1)
+	{
+		__WFI(); //wfi
+	}
+}
+
+/** @} doxygen end group definition */
+
+/*==================[end of file]============================================*/
