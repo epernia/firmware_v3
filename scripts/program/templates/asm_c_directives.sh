43ad92d78f4989e7015ee7b1b141a445b3762641
Luciano Vittori <lucianovittori99@gmail.com>
2020-05-29 23:49:59 -0500

    Mejora templane de asm c directives

--- scripts/program/templates/asm_c_directives.sh
+++ scripts/program/templates/asm_c_directives.sh
@@ -44,6 +44,7 @@ $FILE_HEADER
 
 #include "${PROGRAM_NAME}.h"
 #include "asm_module.h"
+#include "c_function.h"
 #include "sapi.h"
 
 /*=====[Definition macros of private constants]==============================*/
@@ -56,28 +57,38 @@ $FILE_HEADER
 
 /*=====[Main function, program entry point after power on or reset]==========*/
 
-int main( void )
-{
-/*=====[Definitions of private local variables]=============================*/
-	volatile  uint32_t aValue = 20,
-			 otherValue = 30,
-			 sumResult_Asm,sumResult_C;
-			 
-// ----- Setup -----------------------------------
-   	boardInit();
-	
-	sumResult_Asm = asmSum(aValue, otherValue);
+int main(void) {
 
+	/*=====[Definitions of private local variables]=============================*/
+	volatile uint32_t A = 10, B = 20, C = 30, D = 40, sumResult_Asm,
+			sumResult_C;
+
+// ----- Setup -----------------------------------
+	boardInit();
+
+	sumResult_Asm = asmFunction(A, B, C, D);
+	sumResult_C = c_function(A, B, C, D);
 // ----- Repeat for ever -------------------------
-   while( true ) {
+	while ( true) {
 		__WFI(); //wfi
 	}
 
-   // YOU NEVER REACH HERE, because this program runs directly or on a
-   // microcontroller and is not called by any Operating System, as in the 
-   // case of a PC program.
-   return 0;
+	// YOU NEVER REACH HERE, because this program runs directly or on a
+	// microcontroller and is not called by any Operating System, as in the
+	// case of a PC program.
+	return 0;
 }
+EOF
+
+cat <<EOF > ${PB}/src/c_function.c
+$FILE_HEADER
+
+#include "c_function.h"
+
+uint32_t c_function(uint32_t arg0, uint32_t arg1, uint32_t arg2, uint32_t arg3){
+	return (arg0+arg1+arg2+arg3);
+}
+
 EOF
 
 cat <<EOF > ${PB}/src/asm_module.S
@@ -99,7 +110,7 @@ $FILE_HEADER
  * es decir que podemos verlo desde otros modulos (equivalente a extern).
  * Definimos la rutina como global para que sea visible desde otros modulos.
  */
-.global asmSum
+.global asmFunction
 
 /**
  * Indicamos que la siguiente subrutina debe ser ensamblada en modo thumb,
@@ -113,20 +124,21 @@ $FILE_HEADER
 
 #define op1 r0
 #define op2 r1
+#define op3 r2
+#define op4 r3
 
 /*=====[Implementations of public assembly functions]=================================*/
 
 /**
  *	prototipo de la funcion en C
  *
- *	uint32_t asmSum(uint32_t firstOperand, uint32_t secondOperand);
- *
- *	En r0 se cargara firstOperand y en r1 se carga secondOperand. Luego el valor devuelto estara en r0 (si son 32 bits).
- *	Si el resultado que retorna es en 64 bits, usa r0 y r1.
+ *	uint32_t asmFunction(uint32_t arg0, uint32_t arg1, uint32_t arg2, uint32_t arg3);
 */
-asmSum:
+asmFunction:
     push {lr}  /* guardamos la direccion de retorno en la pila */
 	add op1, op2 /* r0 = r0 + r1 */
+	add op1, op3 /* r0 = r0 + r2 */
+	add op1, op4 /* r0 = r0 + r3 */
 	pop {pc}   /* retorno */
 
 	/* otras alternativas para el retorno */
@@ -175,18 +187,39 @@ extern "C" {
 #endif /* __${PROGRAM_NAME_UPPER}_H__ */
 EOF
 
+cat <<EOF > ${PB}/inc/c_function.h
+$FILE_HEADER
+
+#ifndef __${PROGRAM_NAME_UPPER}_C_FUNCTION_H__
+#define __${PROGRAM_NAME_UPPER}_C_FUNCTION_H__
+
+#include "${PROGRAM_NAME}.h"
+
+/**
+ * c_function do the same of ASM function for compare purpose
+ * 
+ * @param arg0
+ * @param arg1
+ * @param arg2
+ * @param arg3
+ * @return
+ */
+uint32_t c_function(uint32_t arg0, uint32_t arg1, uint32_t arg2, uint32_t arg3);
+
+#endif /* __${PROGRAM_NAME_UPPER}_C_FUNCTION_H__ */
+EOF
+
 cat <<EOF > ${PB}/inc/asm_module.h
 $FILE_HEADER
 
 /*=====[Avoid multiple inclusion - begin]====================================*/
 
-#ifndef __ASM_MODULE_H__
-#define __ASM_MODULE_H__
+#ifndef __${PROGRAM_NAME_UPPER}_ASM_MODULE_H__
+#define __${PROGRAM_NAME_UPPER}_ASM_MODULE_H__
 
 /*=====[Inclusions of public function dependencies]==========================*/
 
-#include <stdint.h>
-#include <stddef.h>
+#include "${PROGRAM_NAME}.h"
 
 /*=====[C++ - begin]=========================================================*/
 
@@ -209,12 +242,27 @@ extern "C" {
 /*=====[external functions declaration]======================================*/
 
 /**
- * Funcion que suma dos enteros de 32 bits y devuelve el resultado
- * @param firstOperand primer sumando
- * @param secondOperand segundo sumando
- * @return suma
+ * @brief C prototype function link with asm_module
+ *
+ * Automatically when you call and return from function, MCU utilizes these register:
+ * 
+ * |   C	| CORE    |
+ * | ----- 	| ------- |
+ * |  r0	|  arg0   |
+ * |  r1 	|  arg1   |
+ * |  r2 	|  arg2   |
+ * |  r3 	|  arg3   |
+ * | ----- 	| ------- |
+ * |  r0 	| returnL |
+ * |  r1 	| returnH |
+ *
+ * @param arg0
+ * @param arg1
+ * @param arg2
+ * @param arg3
+ * @return implicit in r0 when return its 32bits, and r1-r0 when is 64bits
  */
-extern uint32_t asmSum(uint32_t firstOperand, uint32_t secondOperand);
+extern uint32_t asmFunction(uint32_t arg0, uint32_t arg1, uint32_t arg2, uint32_t arg3);
 
 /*=====[C++ - end]===========================================================*/
 
@@ -224,5 +272,5 @@ extern uint32_t asmSum(uint32_t firstOperand, uint32_t secondOperand);
 
 /*=====[Avoid multiple inclusion - end]======================================*/
 
-#endif /* __ASM_MODULE__ */
+#endif /* __${PROGRAM_NAME_UPPER}_ASM_MODULE__ */
 EOF
