7b7474cebb7d5fab6065257a416e9362b8a25cb5
Eric Pernia <ericpernia@gmail.com>
2020-08-21 16:02:29 -0300

    add arduino SPI lib

--- libs/arduino/module.mk
+++ libs/arduino/module.mk
@@ -8,11 +8,14 @@ DEFINES+=USE_ARDUINO
 
 INCLUDES += -I$(ARDUINO_BASE)/arduino
 INCLUDES += -I$(ARDUINO_BASE)/arduino/avr
+INCLUDES += -I$(ARDUINO_BASE)/arduino/avr
+INCLUDES += -I$(ARDUINO_BASE)/SPI/inc
 
 SRC += $(wildcard $(ARDUINO_BASE)/arduino/*.c)
 SRC += $(wildcard $(ARDUINO_BASE)/arduino/avr/*.c)
 
 CXXSRC += $(wildcard $(ARDUINO_BASE)/arduino/*.cpp)
+CXXSRC += $(wildcard $(ARDUINO_BASE)/SPI/src/*.cpp)
 #CXXSRC += $(wildcard $(ARDUINO_BASE)/arduino/avr/*.cpp)
 
 # Other Arduino libs
